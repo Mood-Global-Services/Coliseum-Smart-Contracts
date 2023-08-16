0dfee6983564c458f7f8507b11e48344a1cb0dda
sourav maji <majisourav255@gmail.com>
2023-08-16 22:28:53 +0530

    fixing error

--- contracts/Yield.sol
+++ contracts/Yield.sol
@@ -4,20 +4,21 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-// Yield distribution contract
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol"; // Import ERC-721 interface
+import "./ChainlinkBalance.sol"; // Import the ChainlinkBalance contract
 // Yield distribution contract
 contract YieldDistributor {
+    IERC721 public tita; // Use the IERC721 interface for the NFT contract
+    address public usdc;
+    ChainlinkBalance public chainlink;
 
-  Tita public tita;
-  address public usdc;
-  ChainlinkBalance public chainlink;
+    constructor(address _tita, address _usdc, address _chainlink) {
+        tita = IERC721(_tita);
+        usdc = _usdc;
+        chainlink = ChainlinkBalance(_chainlink);
+    }
 
-  constructor(address _tita, address _usdc, address _chainlink) {
-    tita = Tita(_tita);
-    usdc = _usdc; 
-    chainlink = ChainlinkBalance(_chainlink);
-  }
+  event YieldDistributed(address indexed holder, uint256 amount);
 
   function distributeYields() external {
     
@@ -29,7 +30,7 @@ contract YieldDistributor {
     uint256 annualYield = 4 / ownerBalance * 100;
 
     // Take 80% of yield
-    uint256 yieldToDistribute = annualYield * 0.8;
+    uint256 yieldToDistribute = (annualYield * 8) / 10;
 
     // Distribute to holders 
     for (uint256 i = 0; i < tita.totalSupply(); i++) {
